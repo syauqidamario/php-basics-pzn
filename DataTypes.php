12f477b4bbbbb81f1b8cda97eb432783f0ab2d76
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-01-27 16:14:12 +0700

    First commits. First time on basic PHP.

---
+++ DataTypes.php
@@ -0,0 +1,23 @@
+<?php
+
+//Tipe data integer
+echo "Decimal: ";
+var_dump(12.34);
+echo "Octal: ";
+var_dump(0123);
+echo "Hexadecimal: ";
+var_dump(0x1A);
+echo "Binary: ";
+var_dump(01010001);
+echo "Underscore in a number: ";
+var_dump(1_234_567);
+
+//Tipe data floating point
+echo "Floating point: ";
+var_dump(1.999);
+echo "Floating point with E notation plus (1.2 x 1000):";
+var_dump(1.2e3);
+echo "Floating point with E notation minus (7 x 0.001):";
+var_dump(7e-3);
+echo "Underscore in a floating point: ";
+var_dump(1_234.567);
